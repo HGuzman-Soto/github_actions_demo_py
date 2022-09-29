9d4a5890352b5056e3be9692de0fba46bacc81ec
hanselguzman-soto <soto26938@gmail.com>
2022-09-28 18:51:09 -0700

    python workflow

---
+++ github_actions.js
@@ -0,0 +1,18 @@
+const { Octokit } = require("octokit");
+require('dotenv').config()
+
+module.exports = {
+    private_api_key: process.env.private_api_key
+}
+
+const octokit = new Octokit({
+    auth: this.private_api_key
+})
+
+async function test(){
+    await octokit.request("GET /repos/{owner}/{repo}/issues", {
+        owner: "HGuzman-Soto",
+        repo: "github_actions_demo_py",
+    });
+}
+test();
